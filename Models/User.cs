b1cffc3c9a3825436aab5607e2bbe38520019f27
iamkushal2003 <116817277+iamkushal2003@users.noreply.github.com>
2025-12-04 23:39:22 +0530

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,1 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace ComplaintSystem.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required, StringLength(100)]
+        public string FullName { get; set; }
+
+        [Required, EmailAddress, StringLength(150)]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; } // store hashed password
+
+        [Required]
+        public string Role { get; set; } = "User"; // "User" or "Admin"
+
+        public List<Complaint> Complaints { get; set; }
+    }
+}
 
