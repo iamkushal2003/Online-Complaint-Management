679a1d2a83daf945ed0b240bc286fa1dde39ea42
iamkushal2003 <116817277+iamkushal2003@users.noreply.github.com>
2025-12-04 23:40:08 +0530

    Update ComplaintAssignment.cs

--- Models/ComplaintAssignment.cs
+++ Models/ComplaintAssignment.cs
@@ -1,1 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ComplaintSystem.Models
+{
+    public class ComplaintAssignment
+    {
+        public int AssignmentId { get; set; }
+
+        [Required]
+        public int ComplaintId { get; set; }
+        public Complaint Complaint { get; set; }
+
+        [Required]
+        public int TechnicianId { get; set; }
+        public Technician Technician { get; set; }
+
+        public DateTime AssignedAt { get; set; } = DateTime.UtcNow;
+    }
+}
 
