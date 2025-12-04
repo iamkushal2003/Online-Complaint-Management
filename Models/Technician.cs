f23ba631710b635dfd0709b8ea5699cf6d6689d7
iamkushal2003 <116817277+iamkushal2003@users.noreply.github.com>
2025-12-04 23:39:55 +0530

    Update Technician.cs

--- Models/Technician.cs
+++ Models/Technician.cs
@@ -1,1 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ComplaintSystem.Models
+{
+    public class Technician
+    {
+        public int TechnicianId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
 
